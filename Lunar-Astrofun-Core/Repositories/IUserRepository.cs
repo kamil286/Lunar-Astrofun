f0600a64a835b4d7dc27e666d7bba9151909ad7d
kamil286 <siebylakamil@gmail.com>
2018-02-25 17:41:49 +0100

    Added IUserRepository.

---
+++ Lunar-Astrofun-Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Lunar.Astrofun.Core.Domain;
+using System.Collections.Generic;
+
+namespace Lunar.Astrofun.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        User Get(string email);
+        User Get(Guid guid);
+        IEnumerable<User> GetAll();
+        void Add(User user);
+        void Update(User user);
+        void Remove(Guid guid);
+    }
+}
