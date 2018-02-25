8fabc288fa7c25a7e07e99229c2d1cf13b4e5aad
kamil286 <siebylakamil@gmail.com>
2018-02-25 17:51:24 +0100

    Added services and repositories folders to infrastructure project. Implement IUserRepository ito class UserRepository.

---
+++ Lunar-Astrofun-Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Lunar.Astrofun.Core.Domain;
+using Lunar.Astrofun.Core.Repositories;
+
+namespace Lunar.Astrofun.Infrastructure.Repositories
+{
+    class UserRepository : IUserRepository
+    {
+        public void Add(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
