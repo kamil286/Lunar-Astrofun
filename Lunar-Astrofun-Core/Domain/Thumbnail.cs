da9bdd82f41c61cf95aba3c90714442e08380483
kamil286 <siebylakamil@gmail.com>
2018-02-25 14:35:19 +0100

    Added two base models to Core project.

---
+++ Lunar-Astrofun-Core/Domain/Thumbnail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunar.Astrofun.Core.Domain
+{
+    public class Thumbnail
+    {
+        public Guid Id { get; protected set; }
+        public string Title { get; protected set; }
+        public string Category { get; protected set; }
+        public string Content { get; protected set; }
+        public string ImageUrl { get; protected set; }
+        public User CreatedBy { get; protected set; }
+        public DateTime CreatedAt { get; protected set; }
+
+        protected Thumbnail() {}
+
+        public Thumbnail (string title, string category,
+            string content, User createdBy)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Category = category;
+            Content = content;
+            CreatedBy = createdBy;
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
