d8b3a0a29ce8a95c20e1c367a9199548f258e525
kamil286 <siebylakamil@gmail.com>
2018-02-25 14:56:46 +0100

    Added admin domain class to core project.

---
+++ Lunar-Astrofun-Core/Domain/Admin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunar.Astrofun.Core.Domain
+{
+    public class Admin
+    {
+        public Guid Id { get; protected set; }
+        public User AdminPersona { get; protected set; }
+        public IEnumerable<User> RegisteredUsers { get; protected set; }
+        public IEnumerable<Thumbnail> CreatedThumbnails { get; protected set; }
+
+        protected Admin() { }
+
+        public Admin(User adminPersona, IEnumerable<User> registeredUsers,
+            IEnumerable<Thumbnail> createdThumbnails)
+        {
+            Id = Guid.NewGuid();
+            AdminPersona = adminPersona;
+            RegisteredUsers = registeredUsers;
+            CreatedThumbnails = createdThumbnails;
+        }
+    }
+}
