ed36c75cf93703b2cca12b8ea5c1482cf9711dbe
kamil286 <siebylakamil@gmail.com>
2018-02-25 14:08:03 +0100

    Renamed projects.

--- Lunar-Astrofun-API/Controllers/ValuesController.cs
+++ Lunar-Astrofun-API/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Lunar_Astrofun_API.Controllers
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
-        }
+        } 
 
         // GET api/values/5
         [HttpGet("{id}")]
