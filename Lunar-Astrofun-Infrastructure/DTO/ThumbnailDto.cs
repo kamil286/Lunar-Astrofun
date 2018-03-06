8ae781f08c3b528657fd2a7ad6e6741e43c103a9
kamil286 <siebylakamil@gmail.com>
2018-03-06 21:37:42 +0100

    Added ThumbnailDto.

---
+++ Lunar-Astrofun-Infrastructure/DTO/ThumbnailDto.cs
@@ -0,0 +1,17 @@
+﻿using Lunar.Astrofun.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunar.Astrofun.Infrastructure.DTO
+{
+    public class ThumbnailDto
+    {
+        public Guid Id { get; set;}
+        public string Title { get; protected set; }
+        public string Category { get; protected set; }
+        public string Content { get; protected set; }
+        public string ImageUrl { get; protected set; }
+        public Thumbnail CreatedBy { get; protected set; }
+    }
+}
