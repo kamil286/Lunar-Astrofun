5ddc16fd78b7c3f4e14e0906f9a7a1d3dc642131
kamil286 <siebylakamil@gmail.com>
2018-03-06 21:21:25 +0100

    Implemented automapper in user service.

--- Lunar-Astrofun-Infrastructure/Services/UserService.cs
+++ Lunar-Astrofun-Infrastructure/Services/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using AutoMapper;
+using Lunar.Astrofun.Core.Domain;
 using Lunar.Astrofun.Core.Repositories;
 using Lunar.Astrofun.Infrastructure.DTO;
 
@@ -7,36 +9,31 @@ namespace Lunar.Astrofun.Infrastructure.Services
     public class UserService : IUserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UserService(IUserRepository userRepository)
+        public UserService(IUserRepository userRepository, IMapper mapper)
         {
             _userRepository = userRepository;
+            _mapper = mapper;
         }
 
         public UserDto Get(string email)
         {
             var user = _userRepository.Get(email);
-
-            return new UserDto
-            {
-                Id = user.Id,
-                UserName = user.UserName,
-                FullName = user.FullName,
-                Email = user.Email
-        };
-    }
-
-    public void Register(string email, string username, string password)
-    {
-        var user = _userRepository.Get(email);
-        if (user != null)
-        {
-            throw new Exception($"User with email {email} already exist!");
+            return _mapper.Map<User, UserDto>(user);
         }
 
-        var salt = Guid.NewGuid().ToString("N");
-        user = new Core.Domain.User(email, username, password, salt);
-        _userRepository.Add(user);
+        public void Register(string email, string username, string password)
+        {
+            var user = _userRepository.Get(email);
+            if (user != null)
+            {
+                throw new Exception($"User with email {email} already exist!");
+            }
+
+            var salt = Guid.NewGuid().ToString("N");
+            user = new Core.Domain.User(email, username, password, salt);
+            _userRepository.Add(user);
+        }
     }
-}
 }
