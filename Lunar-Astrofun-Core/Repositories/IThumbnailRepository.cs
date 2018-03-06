ea95b3f76f4b1a1f2ff3abddbe2d664c02f0bdaf
kamil286 <siebylakamil@gmail.com>
2018-03-06 21:34:31 +0100

    Added IThumbnailRepository.

---
+++ Lunar-Astrofun-Core/Repositories/IThumbnailRepository.cs
@@ -0,0 +1,16 @@
+﻿using Lunar.Astrofun.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lunar.Astrofun.Core.Repositories
+{
+    public interface IThumbnailRepository
+    {
+        Thumbnail Get(Guid id);
+        IEnumerable<Thumbnail> GetAll();
+        void Add(Thumbnail thumbnail);
+        void Update(Thumbnail thumbnail);
+        void Remove(Guid id);
+    }
+}
